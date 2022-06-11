8dc949c42c74dde13c8e4a6b4093fe687b6cffd2
Akash Jadhav <akash@unibuddy.com>
2022-06-11 16:59:04 +0530

    filter by user id

--- backend/views.py
+++ backend/views.py
@@ -1,12 +1,25 @@
 from rest_framework import viewsets
 from backend.models import VitalData
-
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 from backend.serializers import VitalDataSerializer
 
 
-class VitalDataViewSet(viewsets.ModelViewSet):
-    serializer_class = VitalDataSerializer
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 5
 
-    def get_queryset(self):
-        queryset = VitalData.objects.all()
-        return queryset
+
+class VitalDataViewSet(viewsets.ModelViewSet):
+    pagination_class = StandardResultsSetPagination
+
+    queryset = VitalData.objects.all()
+    serializer_class = VitalDataSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_fields = ["user_id"]
+
+    # def get_queryset(self):
+    #     queryset = VitalData.objects.all()
+    #     return queryset
